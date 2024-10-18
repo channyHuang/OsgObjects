454a2f826ccb10293409c7da6586654e9c6abb36
channyHuang <349117102@qq.com>
2024-10-18 17:33:15 +0800

    fix shader errors

--- imguiShader/main.cpp
+++ imguiShader/main.cpp
@@ -2,177 +2,133 @@
 #include <vector>
 #include <cstring>
 #include <string>
+#include <memory>
+
+#include "shader.h"
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
-#define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#ifdef WIN32
 #include "nativefiledialog/nfd.h"
-#endif
-#include "commonFunc.h"
 
-GLuint vao[1];
-GLuint program;
-GLuint textureID;
+#include <GLFW/glfw3.h>
 
-static void glfw_error_callback(int error, const char* description)
-{
+const int nMaxFileNameLength = 128;
+std::shared_ptr< Shader >    g_pt_shader;
+
+static void glfw_error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-static void createTexture(const unsigned char* data, int width, int height) {
-    const int length = width * height * 4;
-    GLubyte* pixels = new GLubyte[length];
-    memcpy(pixels, data, length * sizeof(char));
-
-
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    free(pixels);
-}
-
-GLuint createShaderProgram() {
-    const char* vShaderSource = Common::readShader("./glsl/vshader.glsl");
-    const char* fShaderSource = Common::readShader("./glsl/fshader.glsl");
-    GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vshader, 1, &vShaderSource, nullptr);
-    glCompileShader(vshader);
-
-    GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fshader, 1, &fShaderSource, nullptr);
-    glCompileShader(fshader);
-
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vshader);
-    glAttachShader(program, fshader);
-    glLinkProgram(program);
-
-    return program;
-}
-
-void init(GLFWwindow* window) {
-    program = createShaderProgram();
-    //glGenVertexArrays(1, vao);
-    //glBindVertexArray(vao[0]);
-}
-
-void display(GLFWwindow* window) {
-    glUseProgram(program);
-    glPointSize(30.f);
-    glDrawArrays(GL_POINTS, 0, 1);
-}
-
-const int nMaxFileNameLength = 128;
-
-int main(int argc, char** argv) {
-    ImVec4 clear_color = ImVec4(0.f, 0.f, 0.f, 1.0f);
-
+GLFWwindow* init_openGL_and_ImGUI(bool bFullScreen = false) {
+    // glfw
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
-        return 1;
+        return nullptr;
     const char* glsl_version = "#version 330";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "PointCloud Reconstruction", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "title", NULL, NULL);
     if (window == NULL)
-        return 1;
-
+        return nullptr;
     glfwMakeContextCurrent(window);
-
-    int state = glewInit();
-    if (state != GLEW_OK) {
-        std::cout << " init failed" << std::endl;
-    }
-
     glfwSwapInterval(1);
-    init(window);
-
+    
+    // imgui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+    if ( bFullScreen ) {
+        glfwMaximizeWindow( window );
+    }
+    
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
     ImGui::StyleColorsDark();
 
+    // combine
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
+    return window;
+}
 
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    
+void initShader() {
+    g_pt_shader = std::make_shared< Shader >(g_sGlslPath + "/glsl/points_shader_rgb.vs", g_sGlslPath + "/glsl/points_shader_rgb.fs");
+}
+
+// 主进程
+int main(int argc, char** argv) {
     char *cFileName = new char[nMaxFileNameLength];
     memset(cFileName, 0, nMaxFileNameLength);
-    int radio = 0;
+    
+    int display_face = 0;
     float Percentage = 1.f;
     bool checkBit = 0;
 
+    GLFWwindow *window = init_openGL_and_ImGUI();
+    if (window == nullptr) {
+        std::cout << "window is nullptr" << std::endl;
+        return -1;
+    }
+    if ( !gladLoadGLLoader( ( GLADloadproc ) glfwGetProcAddress ) ) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    initShader();
+    // render loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-
+        
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // gui
         {
-            ImGui::Begin("point cloud reconstruction");
+            ImGui::Begin("window title here");
 
-            if (ImGui::InputTextWithHint("file", "<.obj .ply .xyz>", cFileName, nMaxFileNameLength, ImGuiInputTextFlags_EnterReturnsTrue)) {
-            }
+            if (ImGui::InputTextWithHint("file", "<.obj .ply .xyz>", cFileName, nMaxFileNameLength, ImGuiInputTextFlags_EnterReturnsTrue)) { }
             ImGui::SameLine();
-#ifdef WIN32
             if (ImGui::Button("Open File")) {
                 nfdresult_t result = NFD_OpenDialog("obj,ply,xyz", nullptr, &cFileName);
-                if (result == NFD_OKAY) {
-                }
+                if (result == NFD_OKAY) { }
             }
-#endif
-            if (ImGui::Button("show model")) {
+
+            if (ImGui::TreeNode("Help")) {
+                ImGui::Text( "[H]    | Display/Close main windows" );
+                ImGui::TreePop();
+                ImGui::Separator();
             }
-            
-            if (ImGui::BeginTabBar("Functions", ImGuiTabBarFlags_None))
-            {
-                if (ImGui::BeginTabItem("Reconstruction"))
+
+            if (ImGui::BeginTabBar("tab begin", ImGuiTabBarFlags_None)) {
+                if (ImGui::BeginTabItem("tab title 1"))
                 {
-                    if (ImGui::Button("cluster")) {
+                    if (ImGui::Button("show or hide points")) {
+                        Eigen::Matrix< double, 4, 4 > proj_mat = Eigen::Matrix< double, 4, 4 >::Identity();
+                        Eigen::Matrix< double, 4, 4 > pose_mat = Eigen::Matrix< double, 4, 4 >::Identity();
+                        g_pt_shader->loadPoints("/home/channy/Documents/datasets_recon/thermocolorlab/scan000.3d");
+                        g_pt_shader->draw(proj_mat, pose_mat);
                     }
-                    ImGui::SeparatorText("smooth");
-                    for (int i = 0; i < 3; ++i) {
-                        ImGui::RadioButton(("radio" + std::to_string(i)).c_str(), &radio, i);
-                    }
+                    ImGui::SeparatorText("draw mesh facet or wireframe");
+
+                    ImGui::RadioButton("facet", &display_face, 0);
+                    ImGui::RadioButton("wireframe", &display_face, 1);
+
                     ImGui::SeparatorText("simplify");
                     ImGui::SliderFloat("remain percentage", &Percentage, 1.0, 99.99);
                     ImGui::SeparatorText("normal");
-                    ImGui::SeparatorText("normalOri");
-                    ImGui::SeparatorText("Reconstruct");
-                    for (int i = 0; i < 3; ++i) {
-                        if (ImGui::Checkbox(("checkbox" + std::to_string(i)).c_str(), &checkBit)) {
-                        }
+                        
+                    if (ImGui::Checkbox(("show xxx"), &checkBit)) {
                     }
 
                     ImGui::EndTabItem();
                 }
-                if (ImGui::BeginTabItem("Unwrap"))
-                {
-                    if (ImGui::Button("show charts")) {
-                    }
 
-                    ImGui::EndTabItem();
-                }
-                if (ImGui::BeginTabItem("test"))
-                {
-                    if (ImGui::Button("show")) {
+                if (ImGui::BeginTabItem("tab title 2")) {
+                    if (ImGui::Button("not implement yeah")) {
                     }
-
                     ImGui::EndTabItem();
                 }
                 ImGui::EndTabBar();
@@ -180,16 +136,9 @@ int main(int argc, char** argv) {
 
             ImGui::End();
         }
+
         ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        display(window);
-
         glfwSwapBuffers(window);
     }
 
---
+++ imguiShader/shader.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+#include "./glad/glad.h"
+#include <Eigen/Core>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#ifdef GLSL_PATH
+std::string g_sGlslPath = GLSL_PATH;
+#endif
+
+struct Point {
+    float m_pos[3];
+    float m_color = 0x000000FF;
+    Point() = default;
+
+    void pack_color(const unsigned char r, const unsigned char g, const unsigned char b)
+    {
+        m_color =  (r << 16) | (g << 8) | b;
+    }
+
+    void pack_color(const float rf, const float gf, const float bf)
+    {
+        unsigned char r = (unsigned char)(rf * 255);
+        unsigned char g = (unsigned char)(gf * 255);
+        unsigned char b = (unsigned char)(bf * 255);
+        m_color =  (r << 16) | (g << 8) | b;
+    }
+
+    Point( const float& x, const float& y, const float& z, const float& rf, const float& gf, const float& bf )
+    {
+        m_pos[ 0 ] = x;
+        m_pos[ 1 ] = y;
+        m_pos[ 2 ] = z;
+        pack_color( ( float ) rf, ( float ) gf, ( float ) bf );
+    }
+
+    Point( const float& x, const float& y, const float& z, const unsigned char& r, const unsigned char& g, const unsigned char& b )
+    {
+        m_pos[ 0 ] = x;
+        m_pos[ 1 ] = y;
+        m_pos[ 2 ] = z;
+        pack_color( r, g, b );
+    }
+};
+
+template < typename T >
+inline glm::mat4 eigen2glm( const Eigen::Matrix< T, 4, 4 >& eigen_mat )
+{
+    glm::mat4 temp_mat;
+    for ( int i = 0; i < 4; i++ )
+    {
+        for ( int j = 0; j < 4; j++ )
+        {
+            temp_mat[ i ][ j ] = eigen_mat( j, i );
+        }
+    }
+    return temp_mat;
+}
+
+std::vector<std::string> splitString(std::string& str, char c) {
+	std::vector<std::string> res;
+	size_t stpos = 0;
+	while (stpos != std::string::npos) {
+		size_t nextpos = str.find_first_of(c, stpos);
+		if (nextpos != std::string::npos) {
+			res.push_back(str.substr(stpos, nextpos - stpos));
+
+			stpos = nextpos + 1;
+		}
+		else {
+			res.push_back(str.substr(stpos));
+			break;
+		}
+	}
+	return res;
+}
+
+class Shader
+{
+public:
+	Shader(const std::string& sVertexShaderFile, const std::string& sFragmentShaderFile) {
+        loadShader(sVertexShaderFile, sFragmentShaderFile);
+    }
+    ~Shader() {
+        glDeleteVertexArrays(1, &m_VAO);
+        glDeleteBuffers(1, &m_VBO);
+    }
+
+    // 读取单个shader文件
+    std::string readShader(const std::string& sShaderFile) {
+        std::string sShaderStr;
+        std::ifstream ifs(sShaderFile.c_str(), std::ios::in);
+        std::stringstream sShaderStream;
+        sShaderStream << ifs.rdbuf();
+        ifs.close();
+        sShaderStr = sShaderStream.str();
+        return sShaderStr;
+    }
+
+    bool checkCompileErrors(GLuint nShaderLoc, std::string sType) {
+        GLint nSuccess = 0;
+        GLchar cInfoLog[1024];
+        if (sType != "PROGRAM") {
+            glGetShaderiv(nShaderLoc, GL_COMPILE_STATUS, &nSuccess);
+            if (!nSuccess) {
+                glGetShaderInfoLog(nShaderLoc, 1024, nullptr, cInfoLog);
+                std::cout << "error: shader compile " << sType << std::endl << " " << cInfoLog << std::endl;
+                return false;
+            }
+        } else {
+            glGetProgramiv(nShaderLoc, GL_LINK_STATUS, &nSuccess);
+            if (!nSuccess) {
+                glGetProgramInfoLog(nShaderLoc, 1024, nullptr, cInfoLog);
+                std::cout << "error: shader compile " << sType << std::endl << cInfoLog << std::endl;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 加载vshader和fshader
+    void loadShader(const std::string& sVertexShaderFile, const std::string& sFragmentShaderFile) {
+        std::ifstream ifsVertex(sVertexShaderFile.c_str(), std::ios::in);
+        std::ifstream ifsFragment(sFragmentShaderFile.c_str(), std::ios::in);
+        std::stringstream sVertexStream, sFragmentStream;
+        sVertexStream << ifsVertex.rdbuf();
+        sFragmentStream << ifsFragment.rdbuf();
+        ifsVertex.close();
+        ifsFragment.close();
+        std::string sVertexCode = sVertexStream.str();
+        std::string sFragmentCode = sFragmentStream.str();
+
+        const char* pVertexCode = sVertexCode.c_str();
+        const char* pFragmentCode = sFragmentCode.c_str();
+
+        GLuint nVertexLoc = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(nVertexLoc, 1, &pVertexCode, nullptr);
+        glCompileShader(nVertexLoc);
+        bool res = checkCompileErrors(nVertexLoc, "VERTEX");
+
+        GLuint nFragmentLoc = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(nFragmentLoc, 1, &pFragmentCode, nullptr);
+        glCompileShader(nFragmentLoc);
+        res = checkCompileErrors(nFragmentLoc, "FRAGMENT");
+
+        m_nProgramID = glCreateProgram();
+        glAttachShader(m_nProgramID, nVertexLoc);
+        glAttachShader(m_nProgramID, nFragmentLoc);
+        glLinkProgram(m_nProgramID);
+        res = checkCompileErrors(m_nProgramID, "PROGRAM");
+
+        glDeleteShader(nVertexLoc);
+        glDeleteShader(nFragmentLoc);
+    }
+
+    void use() {
+        glUseProgram(m_nProgramID);
+    }
+
+    void setUniformFloat(const std::string& sName, float value) {
+        glUniform1f(glGetUniformLocation(m_nProgramID, sName.c_str()), value);
+    }
+
+    void setUniformMat4(const std::string& sName, const glm::mat4& value) {
+        glUniformMatrix4fv(glGetUniformLocation(m_nProgramID, sName.c_str()), 1, GL_FALSE, &value[0][0]);
+    }
+
+    void init_data_buffer() {
+        if (m_bInitBuffer) {
+            glDeleteVertexArrays(1, &m_VAO);
+            glDeleteBuffers(1, &m_VBO);
+        }
+        glGenVertexArrays(1, &m_VAO);
+        glGenBuffers(1, &m_VBO);
+
+        glBindVertexArray(m_VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    
+        int stride = 3 * sizeof(float) + sizeof(float);
+        glBufferData(GL_ARRAY_BUFFER, m_vPoints.size() * (stride), m_vPoints.data(), GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        m_bInitBuffer = true;
+    }
+
+    void draw( const Eigen::Matrix< double, 4, 4 > proj_mat, const Eigen::Matrix< double, 4, 4 > pose_mat, GLenum eMode = GL_POINTS) {
+        int nCount = m_vPoints.size();
+        if (nCount <= 0) return;
+        if (m_bInitBuffer == false) return;
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_BLEND);
+
+        glm::mat4 projection_mul_view = eigen2glm(proj_mat) * eigen2glm(pose_mat);
+        use();
+        setUniformFloat("pointAlpha", 1.0);
+        setUniformFloat("pointSize", 10);
+        setUniformMat4("projection_mul_view", projection_mul_view);
+
+        if (eMode == GL_POINTS) {
+            glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+            glPointSize(10);
+        } else {
+            glEnable(GL_LINE_WIDTH);
+            glLineWidth(10);
+        }
+
+        glBindVertexArray(m_VAO);
+        glDrawArrays(GL_POINTS, 0, nCount);
+        glDepthFunc(GL_LESS);
+    }
+
+    void loadPoints(const std::string& sFileName) {
+        if (m_vPoints.size() > 0) {
+            m_vPoints.clear();
+        }
+        std::ifstream ifs(sFileName, std::ios::in);
+        std::string sLine;
+        while (!ifs.eof()) {
+            std::getline(ifs, sLine);
+            std::vector<std::string> data = splitString(sLine, ' ');
+            if (data.size() < 7) continue;
+            Point pt = Point(std::atof(data[0].c_str()), std::atof(data[1].c_str()), std::atof(data[2].c_str()), (unsigned char)std::atoi(data[4].c_str()), (unsigned char)std::atoi(data[5].c_str()), (unsigned char)std::atoi(data[6].c_str()));
+            m_vPoints.push_back(pt);
+        }
+        ifs.close();
+    }
+
+public:
+    GLuint m_nProgramID;
+    GLuint m_VAO, m_VBO;
+    bool m_bInitBuffer = false;
+    std::vector<Point> m_vPoints;
+};
